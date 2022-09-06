2fecdf8ba4eb0c22065d5294af0ce107905548d7
vincent-the-irritating-git <84185307+vincent-the-irritating-git@users.noreply.github.com>
2022-09-06 11:02:38 +0100

    Delete Constants.cpp

--- Constants.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Constants.h"
-
-	const std::string Pokemon_names::MISSINGNO = "Missingno";
-	const std::string Pokemon_names::PIKACHU = "Pikachu";
-	const std::string Pokemon_names::PINSIR = "Pinsir";
-
-	const int Move_constants::MAX_MOVES = 4;
-	const std::string Move_constants::NULL_MOVE = " ";
